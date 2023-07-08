297dd578189238251a18f062d8d92c41c8a9bd83
RamirezOlmos <nyy_715074@yahoo.com>
2023-07-08 12:17:45 -0600

    no change

--- characters/src/routes/index.js
+++ characters/src/routes/index.js
@@ -1,17 +1,11 @@
-const { Router } = require('express');
-const controllers = require('../controllers');
-const middlewares = require('../middlewares');
-
-
+const { Router } = require("express");
+const controllers = require("../controllers");
+const middlewares = require("../middlewares");
 
 const router = Router();
 
-router.get('/', controllers.getCharacters);
+router.get("/", controllers.getCharacters);
 
-router.post(
-  '/',
-  middlewares.characterValidation,
-  controllers.createCharacter
-);
+router.post("/", middlewares.characterValidation, controllers.createCharacter);
 
 module.exports = router;
