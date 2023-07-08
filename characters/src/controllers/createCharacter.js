f8ada0d68de3cd3ca31be2856146371bd2eebb25
RamirezOlmos <nyy_715074@yahoo.com>
2023-07-08 12:17:20 -0600

    change(characters/src/controllers) change the get and create controllers to use REST API database that uses MONGODB instead of local database.

--- characters/src/controllers/createCharacter.js
+++ characters/src/controllers/createCharacter.js
@@ -1,7 +1,12 @@
-const Character = require('../data');
-const { response } = require('../utils');
+//const Character = require("../data");
+const { response } = require("../utils");
+const axios = require("axios");
 
 module.exports = async (req, res) => {
-  const newCharacter = await Character.create();
-  response(res, 201, newCharacter);
+  const bodyData = req.body;
+  const newCharacter = await axios.post(
+    "http://database:8004/Character",
+    bodyData
+  );
+  response(res, 201, newCharacter.data);
 };
