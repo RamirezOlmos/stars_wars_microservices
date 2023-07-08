d87312d0ad5a3d37453d60b664567f9f19a0f737
RamirezOlmos <nyy_715074@yahoo.com>
2023-07-08 12:33:08 -0600

    has all the changes an created files for the Planet app

--- planets/src/controllers/createPlanet.js
+++ planets/src/controllers/createPlanet.js
@@ -1,7 +1,11 @@
-const Planet = require('../data');
-const { response } = require('../utils');
+//const Planet = require('../data');
+const { response } = require("../utils");
+const axios = require("axios");
 
 module.exports = async (req, res) => {
-  const newPlanet = await Planet.create();
-  response(res, 201, newPlanet);
+  //const newPlanet = await Planet.create();
+  const bodyData = req.body;
+  const newPlanet = await axios.post("http://database:8004/Planet", bodyData);
+  //const newPlanet = await axios.post("http://localhost:8004/Planet", bodyData);
+  response(res, 201, newPlanet.data);
 };
--- planets/src/controllers/getPlanets.js
+++ planets/src/controllers/getPlanets.js
@@ -1,9 +1,10 @@
-const Planets = require('../data');
-const { response } = require('../utils');
-
-
+//const Planets = require('../data');
+const { response } = require("../utils");
+const axios = require("axios");
 
 module.exports = async (req, res) => {
-  const planets = await Planets.list();
-  response(res, 200, planets);
+  //const planets = await Planets.list();
+  const planets = await axios.get("http://database:8004/Planet");
+  //const planets = await axios.get("http://localhost:8004/Planet");
+  response(res, 200, planets.data);
 };
