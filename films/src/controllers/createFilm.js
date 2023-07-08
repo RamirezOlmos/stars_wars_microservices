86e74cd5ef22fcd58101931b6581324122cada8e
RamirezOlmos <nyy_715074@yahoo.com>
2023-07-08 12:32:46 -0600

    has all the changes made to the film app

--- films/src/controllers/createFilm.js
+++ films/src/controllers/createFilm.js
@@ -1,7 +1,11 @@
-const Film = require('../data');
-const { response } = require('../utils');
+//const Film = require('../data');
+const { response } = require("../utils");
+const axios = require("axios");
 
 module.exports = async (req, res) => {
-  const newFilm = await Film.create();
-  response(res, 201, newFilm);
+  //const newFilm = await Film.create();
+  const bodyData = req.body;
+  const newFilm = await axios.post("http://database:8004/Film", bodyData);
+  //const newFilm = await axios.post("http://localhost:8004/Film", bodyData);
+  response(res, 201, newFilm.data);
 };
--- films/src/controllers/getFilms.js
+++ films/src/controllers/getFilms.js
@@ -1,7 +1,10 @@
-const Films = require('../data');
-const { response } = require('../utils');
+//const Films = require('../data');
+const { response } = require("../utils");
+const axios = require("axios");
 
 module.exports = async (req, res) => {
-  const films = await Films.list();
-  response(res, 200, films);
+  //const films = await Films.list();
+  const films = await axios.get("http://database:8004/Film");
+  //const films = await axios.get("http://localhost:8004/Film");
+  response(res, 200, films.data);
 };
