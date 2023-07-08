b0b2d647437d109f3484ab39e01db4f0aa2c85cb
RamirezOlmos <nyy_715074@yahoo.com>
2023-07-08 12:29:01 -0600

    created(database/src/server.js) this is the server file of the REST API of the database. Has the middlewares.

--- database/src/server.js
+++ database/src/server.js
@@ -1,5 +1,19 @@
-const server = require("./src/server");
+const express = require("express");
+const morgan = require("morgan");
 
-server.listen(8004, () => {
-  console.log("Database service on PORT 8004");
+const server = express();
+
+server.use(morgan("dev"));
+server.use(express.json());
+
+server.use("/", require("./routes"));
+
+server.use((err, req, res, next) => {
+  if (err.message === "Invalid model") {
+    res.status(400).send("Not Found");
+  } else {
+    res.status(500).send(err.message);
+  }
 });
+
+module.exports = server;
